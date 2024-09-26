917fd016dece935d31be31f270f27aa086b0278a
Enron Musk <lukerivard17@gmail.com>
2024-09-25 21:34:27 -0400

    Fix IP issue in script.py

---
+++ data_collection/script.py
@@ -0,0 +1,25 @@
+
+import obsws_python as obs
+import os
+from record_mouse import record_mouse_actions
+
+def script():
+    # Connect to OBS
+    ws = obs.ReqClient(host=os.getenv("ip"), port=4455, password='mypassword')  # Update with your WebSocket password if needed
+
+    # Start OBS recording
+    ws.start_record()
+
+    # Start recording mouse actions
+    max_x, max_y = record_mouse_actions(fps=15, duration=12)
+
+    # Stop OBS recording
+    ws.stop_record()
+
+    # Disconnect from OBS
+    ws.disconnect()
+
+    print(f"Recorded mouse data on: width {max_x} height {max_y} screen.")
+
+if __name__ == "__main__":
+    script()

ce597d11d3f5a400a54543b0c4bb9253a37915f0
BYan6667 <graciayan92@gmail.com>
2024-10-17 03:44:15 -0400

    .

--- data/data_collection/synthetic_script.py
+++ data/data_collection/synthetic_script.py
@@ -1,6 +1,10 @@
 from data.data_collection.record_script import record
 import asyncio, time, pyautogui
 from data.data_collection.synthetic_mouse_path import generate_multiple_trajectories, move_mouse_through_trajectory
+import random
+random.seed(1234)
+import numpy as np
+np.random.seed(1234)
 
 async def create_synthetic_dataset(n=1):
     screen_width, screen_height = pyautogui.size()
@@ -10,8 +14,8 @@ async def create_synthetic_dataset(n=1):
     for i, trajectory in enumerate(trajectories):
 
         await record('raw_data', f'record_{i}', move_mouse_through_trajectory, trajectory)
-        time.sleep(1)
+        time.sleep(2)
 
 
 if __name__ == "__main__":
-    asyncio.run(create_synthetic_dataset(1))
+    asyncio.run(create_synthetic_dataset(4))

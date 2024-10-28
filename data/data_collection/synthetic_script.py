f3e5ae13e253fd01089650b5ccc7361183a60f81
yuntian <dengyuntian@gmail.com>
2024-10-28 18:51:29 -0400

    .

--- data/data_collection/synthetic_script.py
+++ data/data_collection/synthetic_script.py
@@ -17,7 +17,7 @@ async def create_synthetic_dataset(n=1):
         await record('raw_data', f'record_{i}', duration=12, 
                     function_to_record=move_mouse_through_trajectory, 
                     fn_args=(trajectory,))
-        time.sleep(2)
+        time.sleep(0.5)
 
 if __name__ == "__main__":
     asyncio.run(create_synthetic_dataset(4000))

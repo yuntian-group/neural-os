667d15070d0dcb45bb9ba22895f0ef1f00f33c8f
da03 <dengyuntian@gmail.com>
2024-11-10 13:41:19 -0500

    .

---
+++ autoencoder/sample.py
@@ -0,0 +1,135 @@
+import numpy as np
+from latent_diffusion.ldm.models.autoencoder import VQModel
+from latent_diffusion.ldm.models.diffusion.ddpm import LatentDiffusion
+from latent_diffusion.ldm.models.diffusion.ddim import DDIMSampler
+import torch, torchvision, argparse
+from omegaconf import OmegaConf
+from einops import rearrange
+import os
+from PIL import Image
+from data.processing.video_convert import create_video_from_frames
+from data.processing.datasets import normalize_image, ActionsData
+from computer.util import init_and_load_model, create_loss_plot, get_mse_image
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+@torch.no_grad()
+def sample_model(model: VQModel, images: list, save_path: str, create_video: bool = False, targets: list = None):
+
+    """
+    Encodes the images and decodes them.
+
+    Parameters:
+        prompts: list of string prompts representing the actions.
+        image_sequences: list of processed images as arrays.
+        create_video: creates a video of the output frames.
+        targets: list of target frames. calculates mse between the generated frames and their ground truth targets (if provided).
+    """
+
+    os.makedirs(save_path, exist_ok=True)
+
+    print("\u2705 Sampling model...")
+
+    generated_frames = []
+    mse_losses = []
+
+    with torch.cuda.amp.autocast(enabled=False):
+        for i,(image) in enumerate(images):
+
+            image = torch.unsqueeze(image, dim=0)
+            image = rearrange(image, 'b h w c -> b c h w').to(device)
+
+            latent = model.encode_to_prequant(image)
+            image = model.decode(latent)
+            if targets: mse_losses.append(get_mse_image(rearrange(image.squeeze(0).cpu(),'c h w -> h w c'), targets[i]))
+
+            image = torch.clamp((image+1.0)/2.0, min=0.0, max=1.0)
+            
+            grid = 255. * rearrange(image.squeeze(0).cpu().numpy(), 'c h w -> h w c')
+            img = Image.fromarray(grid.astype(np.uint8))
+            img.save(os.path.join(save_path, f'sample_{i}.png'))
+
+            generated_frames.append(img)
+
+    if create_video: create_video_from_frames(generated_frames, os.path.join(save_path, 'sample_video.mp4'), fps=15)
+    if targets: create_loss_plot(mse_losses, save_path, title='Sample MSE', dot_plot=True)
+
+@torch.no_grad()
+def sample_model_from_dataset(model: VQModel, dataset: ActionsData, idxs: list, save_path: str, create_video: bool = False, targets: bool = False):
+
+    """
+    Encodes the targets in the dataset from the given indices.
+
+    Parameters:
+        dataset: dataset with __getitem__ method.
+        idxs: items to sample.
+        create_video: creates a video of the output frames.
+        targets: computes mse plot if true.
+    """
+
+    os.makedirs(save_path, exist_ok=True)
+
+    print("\u2705 Sampling model...")
+
+    generated_frames = []
+    mse_losses = []
+
+    with torch.cuda.amp.autocast(enabled=False):
+        for idx in idxs:
+
+            item = dataset.__getitem__(idx)
+
+            image = item['image']
+            target = image
+
+            image = torch.unsqueeze(image, dim=0)
+            image = rearrange(image, 'b h w c -> b c h w').to(device)
+
+            latent = model.encode_to_prequant(image)
+            image = model.decode(latent)
+            if targets: mse_losses.append(get_mse_image(rearrange(image.squeeze(0).cpu(),'c h w -> h w c'), target))
+
+            image = torch.clamp((image+1.0)/2.0, min=0.0, max=1.0)
+            
+            grid = 255. * rearrange(image.squeeze(0).cpu().numpy(), 'c h w -> h w c')
+            img = Image.fromarray(grid.astype(np.uint8))
+            img.save(os.path.join(save_path, f'sample_{idx}.png'))
+
+            generated_frames.append(img)
+
+    if create_video: create_video_from_frames(generated_frames, os.path.join(save_path, 'sample_video.mp4'), fps=15)
+    if targets: create_loss_plot(mse_losses, save_path, title='Sample MSE', dot_plot=True)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Runs the inference script.")
+
+    parser.add_argument("--ckpt_path", type=str, required=True,
+                        help="what model to sample from.")
+    
+    parser.add_argument("--config", type=str, default=os.path.join("autoencoder", "config.yaml"),
+                        help="specifies the model config to load.")
+    
+    parser.add_argument("--image_paths", nargs='+', required=True,
+                        help="A list of image paths.")
+    
+    parser.add_argument("--save_path", type=str, default=os.path.join('autoencoder', 'sample_0'),
+                        help="where to save the output.")
+
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+
+    """
+    Small script to encode and decode a bunch of images.
+    """
+
+    args = parse_args()
+
+    config = OmegaConf.load(args.config)
+    model = init_and_load_model(config, args.ckpt_path)
+    model = model.to(device)
+    
+    images = torch.stack([normalize_image(str(path)) for path in args.image_paths])
+
+    sample_model(model, images, args.save_path)
---
+++ autoencoder/train.py
@@ -0,0 +1,56 @@
+from latent_diffusion.ldm.models.autoencoder import VQModel
+
+from data.processing.datasets import DataModule
+from latent_diffusion.ldm.models.diffusion.ddpm import LatentDiffusion, disabled_train
+from omegaconf import OmegaConf
+from typing import List
+import argparse, os
+from pytorch_lightning.trainer import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+from computer.util import LoggingCallback
+
+def train_model(model: VQModel, data: DataModule, save_path: str, config: OmegaConf, ckpt_path: str) -> VQModel:
+
+    """
+    Trains the decoder layers in the autoencoder on specified dataset in the config.
+    """
+
+    os.makedirs(save_path, exist_ok=True)
+
+    # Define a ModelCheckpoint callback
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=save_path,             # Directory to save checkpoints
+        filename='model_ae_{epoch}',  # Filename format
+        save_top_k=-1,                 # Save all checkpoints, not just the best one
+        save_weights_only=False,       # Save full model (weights + optimizer state)
+        every_n_epochs=1,              # Save every epoch
+    )
+
+    #Disables training for the encoder side.
+    model.encoder.eval()
+    model.encoder.train = disabled_train
+    for param in model.encoder.parameters():
+        param.requires_grad = False
+
+    # configure learning rate
+    base_lr = config.model.base_learning_rate
+    model.learning_rate = base_lr
+
+    lightning_config = config.pop("lightning", OmegaConf.create())
+    trainer_config = lightning_config.get("trainer", OmegaConf.create())
+
+    trainer_opt = argparse.Namespace(**trainer_config)
+
+    print(trainer_opt)
+
+    trainer: Trainer = Trainer(**vars(trainer_opt), callbacks=[checkpoint_callback, LoggingCallback()])
+
+    print("\u2705 Fitting model...")
+
+    trainer.fit(model, data, ckpt_path=ckpt_path)
+    
+    if trainer.is_global_zero: print(f"\u2705 Checkpoints saved at {save_path}")
+    
+    return trainer.model, trainer.is_global_zero
+    

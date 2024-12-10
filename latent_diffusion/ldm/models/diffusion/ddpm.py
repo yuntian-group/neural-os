5c184f9531859f55029ed00c58087d8a974aa20e
da03 <dengyuntian@gmail.com>
2024-12-09 23:52:07 -0500

    .

--- latent_diffusion/ldm/models/diffusion/ddpm.py
+++ latent_diffusion/ldm/models/diffusion/ddpm.py
@@ -871,7 +871,9 @@ class LatentDiffusion(DDPM):
             self.eval()
             #import pdb; pdb.set_trace()
             z_vis = self.decode_first_stage(batch['image_processed'])
+            prev_frames = self.decode_first_stage(batch['c_concat_processed'][:, -4:])
             for i, zz in enumerate(z_vis):
+                prev_frame = prev_frames[i]
                 from PIL import Image
                 import copy
                 #Image.fromarray(((zz.transpose(0,1).transpose(1,2).cpu().float().numpy()+1)*255/2).astype(np.uint8)).save(f'leftclick_debug_image_{i}.png')
@@ -890,9 +892,10 @@ class LatentDiffusion(DDPM):
                 sample_img = ((sample_i[:3].transpose(0,1).transpose(1,2).cpu().float().numpy() + 1) * 127.5).astype(np.uint8)
                 
                 # Create a new image with twice the width to hold both images
-                combined_img = np.zeros((48*8, 64*8*2, 3), dtype=np.uint8)
-                combined_img[:, :64*8] = zz_img  # Original on left
-                combined_img[:, 64*8:] = sample_img  # Generated on right
+                combined_img = np.zeros((48*8, 64*8*3, 3), dtype=np.uint8)
+                combined_img[:, :64*8] = prev_frame  # Original on left
+                combined_img[:, 64*8:64*8*2] = zz_img  # Generated on right
+                combined_img[:, 64*8*2:] = sample_img  # Generated on right
                 
                 # Save the combined image
                 Image.fromarray(combined_img).save(f'real_vs_generated_debug_comparison_{self.i}.png')

a537646a13eff5fcfa058fa3069acf8161e0f763
da03 <dengyuntian@gmail.com>
2025-02-07 01:13:52 +0000

    .

--- computer/run_eval_visualizations.py
+++ computer/run_eval_visualizations.py
@@ -31,8 +31,8 @@ ckpt_dir = 'saved_bsz64_acc1_lr8e5_512_leftclick_histpos_512_384_cont2_ddd_diffi
 ckpt_dir = 'saved_bsz64_acc1_lr8e5_512_leftclick_histpos_512_384_cont2_ddd_difficult_only_withlstmencoder_without_standard_filtered_with_desktop_1.5k_maskprev0_challenging/'
 ckpt_dir = 'saved_bsz64_acc1_lr8e5_512_leftclick_histpos_512_384_cont2_ddd_difficult_only_withlstmencoder_without_standard_filtered_with_desktop_1.5k_maskprev0_challenging_standard/'
 
-#for context_length in [2, 4, 8, 16, 32, 64, 128]:
-for context_length in [2]:
+for context_length in [2, 4, 8, 16, 32, 64]:
+#for context_length in [2]:
     ckpt_dir = f'saved_standard_challenging_context{context_length}'
     print ('='*10)
     print (f'processing context length {context_length}')
@@ -106,7 +106,7 @@ for context_length in [2]:
         
         # Run with modified config (test set)
         try:
-            subprocess.run(f'CUDA_VISIBLE_DEVICES=7 python main.py --config {config_file}', shell=True)
+            subprocess.run(f'CUDA_VISIBLE_DEVICES=4 python main.py --config {config_file}', shell=True)
         except Exception as e:
             print(f"Error in test run: {e}")
             pass

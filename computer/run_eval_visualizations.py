fb615c998385bc7e8c7ce716d35759d467f0ae18
da03 <dengyuntian@gmail.com>
2025-02-12 05:59:07 +0000

    .

--- computer/run_eval_visualizations.py
+++ computer/run_eval_visualizations.py
@@ -32,9 +32,10 @@ ckpt_dir = 'saved_bsz64_acc1_lr8e5_512_leftclick_histpos_512_384_cont2_ddd_diffi
 ckpt_dir = 'saved_bsz64_acc1_lr8e5_512_leftclick_histpos_512_384_cont2_ddd_difficult_only_withlstmencoder_without_standard_filtered_with_desktop_1.5k_maskprev0_challenging_standard/'
 
 #for context_length in [2, 4, 8, 16, 32, 64]:
-for context_length in [4, 8, 16, 32]:
-#for context_length in [2]:
+#for context_length in [4, 8, 16, 32]:
+for context_length in [888]:
     ckpt_dir = f'saved_standard_challenging_context{context_length}'
+    ckpt_dir = f'saved_standard_challenging_context32_nocond_cont_cont_all_cont/'
     print ('='*10)
     print (f'processing context length {context_length}')
     # Get all checkpoint files and sort them
@@ -42,13 +43,14 @@ for context_length in [4, 8, 16, 32]:
     for f in os.listdir(ckpt_dir):
         if f.endswith('.ckpt'):
             step = int(re.search(r'step=(\d+)', f).group(1))
-            if step != 500000:
+            if step != 46000:
                 continue
             ckpts.append((step, f))
     ckpts.sort()  # Sort by step number
     
     # Config file to run
     config_file = f'configs/standard_challenging_context{context_length}.eval.yaml'
+    config_file = f'configs/standard_challenging_context32_nocond_all.eval.yaml'
     
     for step, ckpt in tqdm(ckpts, desc="Processing checkpoints"):
         print(f"Processing checkpoint: {ckpt}")

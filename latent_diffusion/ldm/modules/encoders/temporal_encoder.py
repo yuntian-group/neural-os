2a0ba255edc7b1d0e4581273a8e58d060d0f6356
da03 <dengyuntian@gmail.com>
2025-02-19 04:39:40 -0500

    .

--- latent_diffusion/ldm/modules/encoders/temporal_encoder.py
+++ latent_diffusion/ldm/modules/encoders/temporal_encoder.py
@@ -134,6 +134,7 @@ class TemporalEncoder(nn.Module):
             image_features_with_position = image_features + self.image_position_embeddings
             # apply multi-headed attention to attend lstm_out_lower to image_features_with_position
             context, attention_weights = self.multi_head_attention(lstm_out_lower, image_features_with_position, image_features_with_position, need_weights=True, average_attn_weights=False)
+            context = context + lstm_out_lower
 
             # visualize attention weights and also x and y positions in the same image, but only for the first element in the batch
             if self.num_times % 1000 == 0:

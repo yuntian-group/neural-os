e621b955f38a9299124c5e8b3e3a3810636b7341
da03 <dengyuntian@gmail.com>
2025-02-19 01:29:01 -0500

    .

--- latent_diffusion/ldm/modules/encoders/temporal_encoder.py
+++ latent_diffusion/ldm/modules/encoders/temporal_encoder.py
@@ -19,12 +19,15 @@ class TemporalEncoder(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.output_channels = output_channels
-        self.TRIM_BEGINNING = 1
-        self.TRIM_BEGINNING = 1
-        if self.TRIM_BEGINNING == 1:
-            self.output_channels = output_channels + 2
+        #self.TRIM_BEGINNING = 1
+        #self.TRIM_BEGINNING = 1
+        #if self.TRIM_BEGINNING == 1:
+        #    self.output_channels = output_channels + 2
         self.output_height = output_height
         self.output_width = output_width
+
+        self.initial_state_padding = nn.Parameter(torch.randn(1, hidden_size))
+        self.initial_state_unknown = nn.Parameter(torch.randn(1, hidden_size))
         
         # LSTM to process the sequence
         self.lstm = nn.LSTM(
@@ -46,19 +49,23 @@ class TemporalEncoder(nn.Module):
     # TODO: maybe use a CNN to process the sequence
     # TODO: maybe use aligned images and position maps
     # TODO: maybe use layernorm to preprocess the input
+    #(Pdb) p inputs[0]['image_features'].shape
+
     def forward(self, inputs):
         """
         Args:
             inputs: a list of dictionaries, each containing the following keys:
                 'image_features': Tensor of shape [B, C, H, W]
-                'is_padding': Tensor of shape [B, 1, H, W]
-                'x': Tensor of shape [B, 1, H, W]
-                'y': Tensor of shape [B, 1, H, W]
-                'is_leftclick': Tensor of shape [B, 1, H, W]
+                'is_padding': Tensor of shape [B]
+                'x': Tensor of shape [B, 1]
+                'y': Tensor of shape [B, 1]
+                'is_leftclick': Tensor of shape [B, 1]
         Returns:
             output: Tensor of shape [B, output_channels, output_height, output_width]
         """
+        # initial RNN state: if starts with padding, then use padding state, otherwise use unknown state
         import pdb; pdb.set_trace()
+        initial_states = torch.where(inputs[0]['is_padding'], self.initial_state_padding, self.initial_state_unknown)
         batch_size = x.shape[0]
         sequence_length = x.shape[1]
         assert sequence_length == 14, "Sequence length must be 14"

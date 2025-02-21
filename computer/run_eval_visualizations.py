e72eb01711829b189d66421aff3eb3bdc6fd7062
da03 <dengyuntian@gmail.com>
2025-02-21 00:03:09 +0000

    .

--- computer/run_eval_visualizations.py
+++ computer/run_eval_visualizations.py
@@ -33,13 +33,13 @@ ckpt_dir = 'saved_bsz64_acc1_lr8e5_512_leftclick_histpos_512_384_cont2_ddd_diffi
 
 #for context_length in [2, 4, 8, 16, 32, 64]:
 #for context_length in [4, 8, 16, 32]:
-for context_length in [9999]:
+for context_length in [99999]:
     ckpt_dir = f'saved_standard_challenging_context{context_length}'
     ckpt_dir = f'saved_standard_challenging_context32_nocond_cont_cont_all_cont/'
     ckpt_dir = f'saved_standard_challenging_context32_nocond_fixnorm_all/'
     ckpt_dir = f'saved_standard_challenging_context32_nocond_fixnorm_all_scheduled_sampling_0.2_feedz_comb0.1/'
     ckpt_dir = f'saved_standard_challenging_context32_nocond_fixnorm_all_scheduled_sampling_0.2_feedz_comb0.1_rnn/'
-    ckpt_dir = f'saved_standard_challenging_context32_nocond_fixnorm_all_scheduled_sampling_0.2_feedz_comb0.1_rnn_fixrnn/'
+    ckpt_dir = f'saved_standard_challenging_context32_nocond_fixnorm_all_scheduled_sampling_0.2_feedz_comb0.1_rnn_fixrnn_enablegrad/'
     print ('='*10)
     print (f'processing context length {context_length}')
     # Get all checkpoint files and sort them
@@ -47,7 +47,7 @@ for context_length in [9999]:
     for f in os.listdir(ckpt_dir):
         if f.endswith('.ckpt'):
             step = int(re.search(r'step=(\d+)', f).group(1))
-            if step != 2000:
+            if step != 10000:
                 continue
             ckpts.append((step, f))
     ckpts.sort()  # Sort by step number

c5380007c00649bc7287e1bc1d5eb1f90285c0f2
da03 <dengyuntian@gmail.com>
2025-02-23 15:35:45 -0500

    .

--- data/data_collection/synthetic_mouse_path.py
+++ data/data_collection/synthetic_mouse_path.py
@@ -135,7 +135,7 @@ def generate_human_like_trajectory(screen_width, screen_height,
     active_keys = set()
     up_keys = set()
     right_clicks = np.zeros(len(trajectory), dtype=bool)
-    next_id = 0
+    next_id = 0 + 10
     for i in range(len(trajectory)):
         if random.random() < right_click_prob:
             right_clicks[i] = True
@@ -155,6 +155,7 @@ def generate_human_like_trajectory(screen_width, screen_height,
                 key = random.choice(KEYS)
                 if next_id >= len(KEYS):
                     next_id = 0
+                    print ('resetting next_id')
                 key = KEYS[next_id]
                 next_id += 1
                 

3dc6ea2e7766641235443a608122c69235038e27
Yuntian Deng <yuntian@l3.nibi.sharcnet>
2025-06-24 16:57:56 -0400

    .

--- computer/plot_and_compare_psearch_losses_debug_computecanada.py
+++ computer/plot_and_compare_psearch_losses_debug_computecanada.py
@@ -242,6 +242,8 @@ if __name__ == "__main__":
         ('joint_onlineonly_eps22', 'log.fb.computecanada.challengingandsample.pretrainrnn.balanced.lr5e6.contbest.samplercover.newd.contfreezernn.newnewd.origunet.nospatial2.ONLINE.online.online.x0.joint_onlineonly.eps.2.2.2'),
         ('joint_onlineonly_x02222', 'log.fb.computecanada.challengingandsample.pretrainrnn.balanced.lr5e6.contbest.samplercover.newd.contfreezernn.newnewd.origunet.nospatial2.ONLINE.online.online.x0.joint_onlineonly.2.2.2.2'),
         ('joint_onlineonly_eps2222', 'log.fb.computecanada.challengingandsample.pretrainrnn.balanced.lr5e6.contbest.samplercover.newd.contfreezernn.newnewd.origunet.nospatial2.ONLINE.online.online.x0.joint_onlineonly.eps.2.2.2.2'),
+        ('joint_onlineonly_x022223', 'log.fb.computecanada.challengingandsample.pretrainrnn.balanced.lr5e6.contbest.samplercover.newd.contfreezernn.newnewd.origunet.nospatial2.ONLINE.online.online.x0.joint_onlineonly.2.2.2.2.3'),
+        ('joint_onlineonly_eps22223', 'log.fb.computecanada.challengingandsample.pretrainrnn.balanced.lr5e6.contbest.samplercover.newd.contfreezernn.newnewd.origunet.nospatial2.ONLINE.online.online.x0.joint_onlineonly.eps.2.2.2.2.3'),
     ]
     
     losses_dict = {}

1b86dcc0514caa5f926e48fb508fafdca2d56e52
Yuntian Deng <yuntian@l1.nibi.sharcnet>
2025-07-02 11:28:25 -0400

    .

--- computer/remove_optimizer.py
+++ computer/remove_optimizer.py
@@ -4,10 +4,11 @@ import sys
 def keep_only_state_dict(ckpt_path):
     output_path = ckpt_path + '.state_dict_only'
     # Load checkpoint
-    ckpt = torch.load(ckpt_path, map_location='cpu')
+    #ckpt = torch.load(ckpt_path, map_location='cpu')
+    state_dict = torch.load(ckpt_path, map_location='cpu', weights_only=False, mmap=True)['state_dict']
     
     # Extract state_dict
-    state_dict = ckpt['state_dict']
+    #state_dict = ckpt['state_dict']
     
     # Save the simplified checkpoint
     torch.save({'state_dict': state_dict}, output_path)
